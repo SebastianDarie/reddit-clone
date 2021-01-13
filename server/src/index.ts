afe3c260639c72aab8e758341bd819d92c8275e4
SebastianDarie <s.d.01.2002@gmail.com>
2021-01-13 11:34:11 +0200

    mikro-orm typegraphql crud

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,34 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
+//import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  // const post = orm.em.create(Post, { title: 'first post' });
-  // await orm.em.persistAndFlush(post);
+  const app = express();
 
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('Server running on port 4000');
+  });
 };
 
-main().catch(err=> {
+main().catch((err) => {
   console.error(err);
 });
